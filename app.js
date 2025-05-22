6502c61cee2596fad99b75bec98a29c75df7cbee
KarthikEdhala <117622559+KarthikEdhala@users.noreply.github.com>
2025-05-22 11:36:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://karthikedhala:Dm44eOmR6AZkqtHF@cluster0.g9ocq8b.mongodb.net/aliensDB?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const connectToMongo = async () => {
+  if (mongoose.connection.readyState === 1) {
+    console.log("✅ Mongo already connected");
+    return;
   }
-);
-const con = mongoose.connection;
 
-con.on("open", () => {
-  console.log("connected...");
+  await mongoose.connect(
+    "mongodb+srv://karthikedhala:Dm44eOmR6AZkqtHF@cluster0.g9ocq8b.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+  console.log("✅ Mongo connected now");
+};
+
+// ✅ Attach DB connection before each Lambda request
+app.use(async (req, res, next) => {
+  await connectToMongo();
+  next();
 });
 
 app.use(express.json());
