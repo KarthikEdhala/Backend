c788200776087981382247532308c97ef40877ed
Edhala <karthik@aios.agency>
2025-05-23 23:39:35 -0400

    push

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({ origin: "*" }));
 
 const connectToMongo = async () => {
   if (mongoose.connection.readyState === 1) {
